b62c2dce7118ca89bb73245a74f22c0fa789b21c
VictorMaria <victor.abayomi@outlook.com>
2020-01-31 17:40:21 +0100

    chore(setup): Set up test and dev environment initialise express server [Finishes #171018982]

---
+++ config/db.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+ 
+dotenv.config();
+const { NODE_ENV, mongoURITest, mongoURI } = process.env;
+const selectMongoURI = () => {
+    if (NODE_ENV === 'test') {
+        const URIString = mongoURITest;
+        return URIString;
+    }
+    const URIString = mongoURI
+    return URIString
+};
+const db = selectMongoURI();
+ 
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+ 
+export default connectDb;
---
+++ index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import '@babel/polyfill';
+import express from 'express';
+import Debug from 'debug';
+import cors from 'cors';
+import connectDb from './config/db';
+ 
+const ApiPrefix = '/api/v1';
+const debug = Debug('dev');
+ 
+const app = express();
+connectDb();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+ 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Premier League lives here' });
+});
+ 
+const PORT = process.env.PORT || 5000;
+ 
+app.listen(PORT, () => debug(`Premier League lives on ${PORT}`));
+ 
+export default app;
